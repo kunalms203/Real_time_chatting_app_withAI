e5e699d104bf7595fddc64e03860f29d59e8beec
kunalms203 <ksreviews8@gmail.com>
2025-06-08 21:41:29 +0530

    Added new url to socket 5

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import { app } from "../lib/socket.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
