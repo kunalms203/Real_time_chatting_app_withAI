545b91a21fac92b570140a9755c60991551012f7
kunalms203 <ksreviews8@gmail.com>
2025-06-08 17:10:03 +0530

    Created  deployment url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://finalyearproject-updated-backend2.onrender.com/api" : "https://finalyearproject-updated-backend2.onrender.com/api",
   withCredentials: true,
 });
