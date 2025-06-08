1ebccc933a62914384cab964975c1acd44f5269e
kunalms203 <ksreviews8@gmail.com>
2025-06-08 19:32:39 +0530

    Added new url to socket 4

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,9 +7,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://finalyearproject-updated.onrender.com"],
+    origin: ["http://localhost:5173", "https://finalyearproject-updated.onrender.com"],
+    credentials: true,
   },
 });
+
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
